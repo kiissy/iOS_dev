95bf4b617bf70af97de919e6d0810af85345e541
kiissy <kiissy@naver.com>
2022-07-14 13:08:23 +0900

    commit

---
+++ Web/Web/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Web
+//
+//  Created by mbp13 on 2022/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
